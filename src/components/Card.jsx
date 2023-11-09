ba4241acef45d0dbddc8d4cdc0c1f6c89cf584d1
Sebaplz <sebastian.h.neira@outlook.com>
2023-11-09 17:03:06 -0300

    Nav mobile

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,7 @@ export default function Card() {
           className={`absolute -top-10 left-1/2 h-[85px] w-[85px] -translate-x-1/2 rounded-full bg-[#3B3054] bg-[url('/icon-brand-recognition.svg')] bg-center bg-no-repeat`}
         ></div>
         <h3 className="pb-4 pt-10 text-2xl font-bold">Brand Recognition</h3>
-        <p className="text-[#BFBFBF]">
+        <p className="pb-8 text-[#BFBFBF]">
           Boost your brand recognition with each click. Generic links don&apos;t
           mean a thing. Branded links help instil confidence in your content.
         </p>
@@ -17,7 +17,7 @@ export default function Card() {
           className={`absolute -top-10 left-1/2 h-[85px] w-[85px] -translate-x-1/2 rounded-full bg-[#3B3054] bg-[url('/icon-detailed-records.svg')] bg-center bg-no-repeat`}
         ></div>
         <h3 className="pb-4 pt-10 text-2xl font-bold">Detailed Records</h3>
-        <p className="text-[#BFBFBF]">
+        <p className="pb-8 text-[#BFBFBF]">
           Gain insights into who is clicking your links. Knowing when and where
           people engage with your content helps inform better decisions.
         </p>
@@ -28,7 +28,7 @@ export default function Card() {
           className={`absolute -top-10 left-1/2 h-[85px] w-[85px] -translate-x-1/2 rounded-full bg-[#3B3054] bg-[url('/icon-fully-customizable.svg')] bg-center bg-no-repeat`}
         ></div>
         <h3 className="pb-4 pt-10 text-2xl font-bold">Fully Customizable</h3>
-        <p className="text-[#BFBFBF]">
+        <p className="pb-8 text-[#BFBFBF]">
           Improve brand awareness and content discorverability through
           customizable links, supercharging audience engagement.
         </p>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,46 @@
+import { useState } from "react";
 import LogoSVG from "../icons/LogoSVG";
 
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   const handleMenu = () => {
-    console.log("Click en el boton del menu");
+    setIsMenuOpen(!isMenuOpen);
   };
+
   return (
     <header className="px-4 py-8">
       <nav className="flex items-center justify-between">
         <LogoSVG fill={"#232127"} />
-        <button onClick={handleMenu}>
-          <img src="/menu.svg" alt="svg de menu" className="h-8 w-8" />
+        <button onClick={handleMenu} className="block lg:hidden">
+          <img src="/menu.svg" alt="svg de menú" className="h-8 w-8" />
         </button>
+
+        {/* Menú */}
+        <ul
+          className={`absolute top-20 flex w-[90%] flex-col items-center gap-6 rounded-lg bg-[#3B3054] py-8 text-white md:w-[95%] ${
+            isMenuOpen ? "" : "hidden"
+          }`}
+        >
+          <li>
+            <a href="#">Features</a>
+          </li>
+          <li>
+            <a href="#">Pricing</a>
+          </li>
+          <li>
+            <a href="#">Resources</a>
+          </li>
+          <div className="h-[1px] w-[80%] bg-[#BFBFBF]"></div>
+          <li>
+            <a href="#">Login</a>
+          </li>
+          <li>
+            <button className="w-[280px] rounded-full bg-[#2ACFCF] p-2 text-white">
+              Sign Up
+            </button>
+          </li>
+        </ul>
       </nav>
     </header>
   );

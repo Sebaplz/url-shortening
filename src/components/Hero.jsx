f7f723f2cc6a43fb60785d48e7337a2ebfcb5618
Sebaplz <sebastian.h.neira@outlook.com>
2023-11-09 17:08:32 -0300

    md listo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,12 @@
 export default function Hero() {
   return (
     <section id="hero">
-      <div className="bg-left- h-[350px] w-full bg-[url('/illustration-working.svg')] bg-cover bg-[15px] bg-no-repeat"></div>
-      <div className="flex flex-col items-center  gap-4 pt-4 text-center">
+      <div className="h-[350px] w-full bg-[url('/illustration-working.svg')] bg-cover bg-[15px] bg-no-repeat md:h-[400px] md:bg-contain md:bg-right"></div>
+      <div className="flex flex-col items-center gap-4 pt-4 text-center">
         <h1 className="text-4xl font-bold text-[#232127]">
           More than just shorter links
         </h1>
-        <p className="text-[#BFBFBF]">
+        <p className="px-10 text-[#BFBFBF]">
           Build your brand&apos;s recognition and get detailed insights on how
           your links are performing
         </p>
